6c97aaaa680839969a538101c47e58f918c68585
CHAITU <chaituzma@gmail.com>
2019-08-31 23:31:21 +0530

    Modified SecondProgram from chaitubranch

---
+++ src/main/java/GitPackage/GitProject/SecondProgram.java
@@ -0,0 +1,8 @@
+package GitPackage.GitProject;
+
+public class SecondProgram {
+	public static void main(String[] args) {
+		System.out.println("This is my Second Program.");
+		System.out.println("Added second line to print.");
+	}
+}
