1bb8b87993e933a7936f2c24921f5e82c375989b
CHAITU <chaituzma@gmail.com>
2019-08-31 21:50:35 +0530

    Pushing the final .gitignore file with first program

---
+++ src/main/java/GitPackage/GitProject/App.java
@@ -0,0 +1,13 @@
+package GitPackage.GitProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/GitPackage/GitProject/HelloWorld.java
@@ -0,0 +1,8 @@
+package GitPackage.GitProject;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World !");
+	}
+
+}
