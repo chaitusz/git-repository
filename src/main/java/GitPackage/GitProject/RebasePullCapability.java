67c3e55a40e8f321a2f2110dd335fcf3b480713a
Chaitanya <chaituzma@gmail.com>
2020-02-09 21:20:23 +0530

    adding super file

---
+++ src/main/java/GitPackage/GitProject/RebasePullCapability.java
@@ -0,0 +1,14 @@
+package GitPackage.GitProject;
+
+public class RebasePullCapability {
+
+	public void method1() {
+		System.out.println("I am from method1 created locally");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
