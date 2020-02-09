f50c68583a767d2d7f67d66a7d738925575cb2b1
Chaitanya <chaituzma@gmail.com>
2020-02-09 21:34:27 +0530

    adding method5 local

--- src/main/java/GitPackage/GitProject/RebasePullCapability.java
+++ src/main/java/GitPackage/GitProject/RebasePullCapability.java
@@ -5,7 +5,6 @@ public class RebasePullCapability {
 	public void method1() {
 		System.out.println("I am from method1 created locally");
 	}
-<<<<<<< HEAD
 	
 	public void method2(){
 		System.out.println("I am from method2 created remotely");
@@ -15,17 +14,19 @@ public class RebasePullCapability {
 		System.out.println("I am from method3 created remotely");
 	}
 	
-=======
 
 	public void method4() {
 		System.out.println("I am from method4 created locally");
 	}
 
->>>>>>> added method4 locally
 	
 	public void method6(){
 		System.out.println("I am from method6 created remotely");
 	}
+	public void method5() {
+		System.out.println("I am from method5 created locally");
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
