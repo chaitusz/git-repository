0188ce8455fbdcd75f74f8c102bbaaa94e66238f
Chaitanya <30646816+chaitusz@users.noreply.github.com>
2020-02-09 21:21:25 +0530

    Update RebasePullCapability.java

--- src/main/java/GitPackage/GitProject/RebasePullCapability.java
+++ src/main/java/GitPackage/GitProject/RebasePullCapability.java
@@ -5,7 +5,11 @@ public class RebasePullCapability {
 	public void method1() {
 		System.out.println("I am from method1 created locally");
 	}
-
+	
+	public void method2(){
+		System.out.println("I am from method2 created remotely");
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
