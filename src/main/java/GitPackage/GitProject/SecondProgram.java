5f83c966d81834edaf03f545d3e40310885c99c3
Chaitanya <chaituzma@gmail.com>
2020-02-09 21:18:09 +0530

    undoing the previous remove

---
+++ src/main/java/GitPackage/GitProject/SecondProgram.java
@@ -0,0 +1,10 @@
+package GitPackage.GitProject;
+
+public class SecondProgram {
+	public static void main(String[] args) {
+		System.out.println("This is my Second Program.");
+		System.out.println("Added second line to print.");
+		System.out.println("Changing now from Master Branch");
+		System.out.println("Changing now from chaitubranch");
+	}
+}
